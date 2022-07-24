63f03b257a402348a4a6c53b4f79a6778fbbfd06
Qstar12 <ajones11189@gmail.com>
2022-07-24 18:54:41 -0400

    project3

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
-const btn = document.querySelector('readmore-btn');
-const snippet = document.querySelector('#snippet');
-
-$(function() {
-    $('.readmore-btn').on('click', function() {
-        $(this).parent().toggleClass('showContent')
-    });
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    alert('Hello World!');
 })
+
+window.onload = () => {
+    setTimeout(() => {
+        alert('Thanks for viewing this page! To get started, click the "Get Started" button.');
+    }, 1000);
+}
